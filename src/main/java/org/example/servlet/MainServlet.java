39bd5f231ddb9e4e290532f9a8348a60736ab78a
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2025-12-15 16:51:56 +0300

    initial commit

---
+++ src/main/java/org/example/servlet/MainServlet.java
@@ -0,0 +1,57 @@
+package org.example.servlet;
+
+import org.example.controller.PostController;
+import org.example.repository.PostRepository;
+import org.example.service.PostService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+
+    @Override
+    public void init() throws ServletException {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // super.service(req, resp);
+
+        // resp.setContentType("text/plain");
+        // resp.getWriter().print("ok");
+
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+
+            if (method.equals("GET") && path.equals("/api/posts")) {
+                controller.all(resp);
+                return;
+            }
+            if(method.equals("GET") && path.matches("/api/posts/\\d+")) {
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals("POST") && path.equals("/api/posts")) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
